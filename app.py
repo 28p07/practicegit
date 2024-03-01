7ce014c80824c4512be54469f5e3d3082419169d
Puneet Sharma <sharmapuneet8126@gmail.com>
2024-03-01 12:11:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 def add(a,b,c):
     return a+b+c
-
-<<<<<<< HEAD
-def multiply(a,b,d):
-    return a*b*d
-=======
+    
 def multiply(a,b,e):
     return a*b*e
->>>>>>> d0424e85a7550aa9b8a1f8ffca05ad465268ba57
 
 def divide(a,b):
     return a/b
